398292b40595b1f5db4deb732a05bf0eb863c93a
wiztemple <wiztemple7@gmail.com>
2021-06-02 17:37:32 +0100

    add latest changes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,9 @@ import MenuItem from '../../components/MenuItem';
 import Navbar from '../../components/Navbar';
 import { ThemeContext } from '../../context/themeContext';
 
+// extra styles
+import '../../App.css';
+
 const menuItems = {
   Top: 'Home',
   Section1: 'Projects',
@@ -88,13 +91,22 @@ const Home = () => {
             <div className="md:flex pt-5 md:pt-12">
               <div className="md:w-1/2 md:flex">
                 <div className="flex-1">
-                  <div className="relative">
+                  <div className="relative recentcard">
                     <img
                       className="h-275px w-full object-cover"
                       src="https://images.pexels.com/photos/87223/pexels-photo-87223.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
                       alt="house construcion"
                     />
-                    <div className="absolute h-full w-full bg-black bg-opacity-40 top-0">s</div>
+                    <div className="absolute h-full w-full top-0 recentoverlay">
+                      <div className="flex items-center space-x-4 link-holder">
+                        <h1>Skyscrapers</h1>
+                        <a href="a" className="flex justify-center items-center bg-white h-10 w-10 rounded-full">
+                          <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" clipRule="evenodd" d="M1.5625 12.5C1.5625 12.2928 1.64481 12.0941 1.79132 11.9476C1.93784 11.8011 2.13655 11.7188 2.34375 11.7188H20.7703L15.8531 6.80314C15.7064 6.65644 15.624 6.45747 15.624 6.25001C15.624 6.04255 15.7064 5.84358 15.8531 5.69689C15.9998 5.55019 16.1988 5.46777 16.4063 5.46777C16.6137 5.46777 16.8127 5.55019 16.9594 5.69689L23.2094 11.9469C23.2821 12.0195 23.3399 12.1057 23.3792 12.2006C23.4186 12.2955 23.4389 12.3972 23.4389 12.5C23.4389 12.6028 23.4186 12.7045 23.3792 12.7994C23.3399 12.8943 23.2821 12.9806 23.2094 13.0531L16.9594 19.3031C16.8127 19.4498 16.6137 19.5322 16.4063 19.5322C16.1988 19.5322 15.9998 19.4498 15.8531 19.3031C15.7064 19.1564 15.624 18.9575 15.624 18.75C15.624 18.5425 15.7064 18.3436 15.8531 18.1969L20.7703 13.2813H2.34375C2.13655 13.2813 1.93784 13.1989 1.79132 13.0524C1.64481 12.9059 1.5625 12.7072 1.5625 12.5V12.5Z" fill="black" />
+                          </svg>
+                        </a>
+                      </div>
+                    </div>
                   </div>
                   <div className="relative">
                     <img
